343c4c04a235a5198a3cc35105f5afcc00df7002
Sean Yang <53060248+shawseanyang@users.noreply.github.com>
2024-07-30 17:16:42 +0200

    timeline and network graph

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import styled from 'styled-components';
+import Container from 'react-bootstrap/Container';
+
+// Container that takes up the full height of the viewport, with no padding or margin, and places its children in a column
+const FullHeightSeemlessColumnContainer = styled(Container)`
+  display: flex;
+  flex-direction: column;
+  padding: 0 !important;
+  margin: 0 !important;
+  height: 100vh;
+  width: 100vh;
+  max-height: 100vh !important;
+  max-width: 100vw !important;
+`;
+
+const TimelinePlaceholder = styled.div`
+  height: 200px; // Fixed height for the timeline
+  background-color: #969696;
+  
+  // for the placeholder text:
+  text-align: center;
+  line-height: 200px;
+`;
+
+const NetworkGraphPlaceholder = styled.div`
+  flex-grow: 1; // Take up remaining space left over from timeline
+  background-color: #d4d4d4;
+
+  // for the placeholder text:
+  text-align: center;
+  // vertically align the placeholder text
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
-
-  useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-
-        ... no changes in this part ...
-
-        <p>The current time is {currentTime}.</p>
-      </header>
-    </div>
+    <FullHeightSeemlessColumnContainer>
+      <TimelinePlaceholder>
+        Timeline
+      </TimelinePlaceholder>
+      <NetworkGraphPlaceholder>
+        Network Graph
+      </NetworkGraphPlaceholder>
+    </FullHeightSeemlessColumnContainer>
   );
 }
 
