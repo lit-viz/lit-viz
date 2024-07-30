bf1b368595c6a5d3043ec4cb0a1fa55db0f58371
Sean Yang <53060248+shawseanyang@users.noreply.github.com>
2024-07-30 17:24:56 +0200

    offcanvas

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import styled from 'styled-components';
 import Container from 'react-bootstrap/Container';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 // Container that takes up the full height of the viewport, with no padding or margin, and places its children in a column
 const FullHeightSeemlessColumnContainer = styled(Container)`
@@ -36,16 +37,51 @@ const NetworkGraphPlaceholder = styled.div`
   align-items: center;
 `;
 
+// Handle on the left side of the screen that opens an offcanvas
+const OffcanvasHandle = styled.div`
+  position: absolute;
+  top: 50%;
+  right: 0;
+  transform: translate(100%, -50%);
+  background-color: #f8f9fa;
+  padding: 0.5rem;
+  border-radius: 0 0.25rem 0.25rem 0;
+  border: 1px solid #ced4da;
+  border-right: 0;
+  cursor: pointer;
+  z-index: 1051;
+`;
+
 function App() {
+
+  const [show, setShow] = useState(true);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <FullHeightSeemlessColumnContainer>
-      <TimelinePlaceholder>
-        Timeline
-      </TimelinePlaceholder>
-      <NetworkGraphPlaceholder>
-        Network Graph
-      </NetworkGraphPlaceholder>
-    </FullHeightSeemlessColumnContainer>
+    <>
+      <Offcanvas show={show} onHide={handleClose} backdrop={false}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          Some text as placeholder
+        </Offcanvas.Body>
+        <OffcanvasHandle onClick={handleShow}>
+        &gt;
+        </OffcanvasHandle>
+      </Offcanvas>
+      <FullHeightSeemlessColumnContainer>
+        <TimelinePlaceholder>
+          Timeline
+        </TimelinePlaceholder>
+        <NetworkGraphPlaceholder>
+          Network Graph
+        </NetworkGraphPlaceholder>
+      </FullHeightSeemlessColumnContainer>
+    </>
+    
   );
 }
 
