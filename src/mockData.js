974415f7614fcb0ef6789c9cc632a06fcdf07af6
Sean Yang <53060248+shawseanyang@users.noreply.github.com>
2024-08-17 15:02:39 +0200

    add mock data

---
+++ src/mockData.js
@@ -0,0 +1,93 @@
+export const LETTERS = [
+  {
+    sender: 'John Doe',
+    recipient: 'Jane Smith',
+    length_words: 500,
+    date: new Date('1890-05-01'),
+    sentiment: 0.9,
+    raw_text: 'Lorem ipsum dolor',
+    annotations: []
+  },
+  {
+    sender: 'Alice Johnson',
+    recipient: 'Bob Williams',
+    length_words: 500,
+    date: new Date('1905-01-05'),
+    sentiment: 0.25,
+    raw_text: 'Lorem ipsum dolor',
+    annotations: []
+  },
+  {
+    sender: 'Emily Davis',
+    recipient: 'Michael Brown',
+    length_words: 500,
+    date: new Date('1893-03-21'),
+    sentiment: 0.1,
+    raw_text: 'Lorem ipsum dolor',
+    annotations: []
+  },
+  {
+    sender: 'Sarah Wilson',
+    recipient: 'David Thompson',
+    length_words: 500,
+    date: new Date('1891-11-01'),
+    sentiment: 0.6,
+    raw_text: 'Lorem ipsum dolor',
+    annotations: []
+  },
+  {
+    sender: 'James Anderson',
+    recipient: 'Olivia Taylor',
+    length_words: 500,
+    date: new Date('1898-08-04'),
+    sentiment: 0.4,
+    raw_text: 'Lorem ipsum dolor',
+    annotations: []
+  },
+  // add some more between the existing authors
+  {
+    sender: 'John Doe',
+    recipient: 'Alice Johnson',
+    length_words: 500,
+    date: new Date('1890-05-01'),
+    sentiment: 0.9,
+    raw_text: 'Lorem ipsum dolor',
+    annotations: []
+  },
+  {
+    sender: 'Alice Johnson',
+    recipient: 'Emily Davis',
+    length_words: 500,
+    date: new Date('1905-01-05'),
+    sentiment: 0.25,
+    raw_text: 'Lorem ipsum dolor',
+    annotations: []
+  },
+  {
+    sender: 'Emily Davis',
+    recipient: 'Sarah Wilson',
+    length_words: 500,
+    date: new Date('1893-03-21'),
+    sentiment: 0.1,
+    raw_text: 'Lorem ipsum dolor',
+    annotations: []
+  },
+  {
+    sender: 'Sarah Wilson',
+    recipient: 'James Anderson',
+    length_words: 500,
+    date: new Date('1891-11-01'),
+    sentiment: 0.6,
+    raw_text: 'Lorem ipsum dolor',
+    annotations: []
+  },
+  {
+    sender: 'James Anderson',
+    recipient: 'John Doe',
+    length_words: 500,
+    date: new Date('1898-08-04'),
+    sentiment: 0.4,
+    raw_text: 'Lorem ipsum dolor',
+    annotations: []
+  },
+];
